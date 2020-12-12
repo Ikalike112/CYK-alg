feb8f1cf7995eb0295048266ff5c66ef29e48fab
1183735 <1183735@K408-08>
2020-12-12 10:06:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Вводим строку алгоритма");
+            str = Console.ReadLine();//входная строка длинной n
+            char[,] M = new char[str.Length,str.Length];//треугольная матрица n-ого порядка
+            string[][] s = new string[str.Length][];
+            for (int i=str.Length;i>0;i++)
+            s[0] = new string[i];
+            
+
+        }
+    }
+}
